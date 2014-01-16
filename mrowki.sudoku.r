c3c6b10f3f39b70c10adcf61fb1c840fefd41841
servlok <jakubstrzalek@gmail.com>
2014-01-16 20:31:10 +0100

    zmiany w plikach: + mrowki.sudoku.get_pheromons + mrowki.sudoku.add_son - wszystkie funkcje zwiazane z ATRAKCYJNOSCIA
    przeniesiony zostal:
    sudoku.set_square_iter_seq do mrowki.r z mrowki.sudoku.r

--- mrowki.sudoku.r
+++ mrowki.sudoku.r
@@ -4,11 +4,6 @@ source('sudoku.r')
 #model init jest w mrowkach, model init czesciowo korzysta z funkcji
 # ktore sa w mrowki.sudoku, ale sama funkcja jest w mrowkach
 
-mrowki.sudoku.identity_number <- function() {
-  return(1:729)
-}
-
-
 mrowki.sudoku.init_state <- function() {
 #  s = c()
 #  s[1:9] =   c(0,0,0,0,0,0,3,1,0)
@@ -25,185 +20,27 @@ mrowki.sudoku.init_state <- function() {
   return(s)
 }
 
-mrowki.sudoku.set_numb_seq <<- sudoku.set_numb_seq
-mrowki.sudoku.check_raw <<- sudoku.check_raw
-mrowki.sudoku.check_column <<- sudoku.check_column
-mrowki.sudoku.check_square <<- sudoku.check_square
-
 mrowki.sudoku.extend_state <- function(s) {
   seq = array(data=list(), dim = c(9,9))
   for(i in 1:9) {
     for(j in 1:9) {
       if(s[j,i] == 0) {
-        seq[j,i][[1]] <- mrowki.sudoku.set_numb_seq(i,j,s)
+        seq[j,i][[1]] <- sudoku.set_numb_seq(i,j,s)
       } 
     }
   }
   return(seq)
 }
 
-
-
-
-mrowki.sudoku.set_square_iter_seq <- function(position) {
-  temp <- position %% 3 
-  if(temp == 0) { 
-    return((position-2):position) 
-  }
-  if(temp == 1) {
-    return(position:(position+2) )
-  }
-  if(temp == 2) {
-    return((position-1):(position+1))
-  }
-  stop("Wystapil blad!")
-  return(c())
-}
-
-# funkcja uzyta do wyznaczania atrakcyjnosci to 1/x^WA gdzie x to ilosc
-# mozliwych podstawien 
-mrowki.sudoku.set_attractivity <- function(pn,WA=4) {
-  attr = array(data=0, dim = c(9,9)) 
-  
-  for(i in 1:9) {
-    for(j in 1:9) {
-      if(length(pn[i,j][[1]]) != 0)
-        attr[i,j] = 1/(length(pn[i,j][[1]]))^WA
-    }
-  }
-  
-  return (attr)
-}
-
 mrowki.sudoku.there_is_move <- function(id_sons) {
-  if(id_sons == list())
+  if(length(sons) == 0)
     return (FALSE)
   
   return (TRUE)
 }
 
-# 
-mrowki.sudoku.set_propability_matrix <- function(attr, pn, pheromons) { 
-  # pher = array(data=0, dim=c(9,9,9)) # stub chwilowy
-  prop_array = array(data=0, dim=c(9,9,9))
-  
-  pheromons <- pheromons/sum(pheromons) # normalizacja
-  attr <- attr/sum(attr)
-
-  
-  for(i in 1:9) {
-    for(j in 1:9) {
-      for(k in pn[i,j][[1]]) {
-        prop_array[i,j,k] = pheromons[i,j,k] + attr[i,j]
-      }
-    }
-  }
-  
-  return (prop_array)
-}
-
 mrowki.sudoku.rand_move <- function(Elements,P) {
-  return (sample(x=Elements,size=1,replace=FALSE,prob=P))
-}
-
-mrowki.sudoku.get_rand_column <- function(numb) {
-  return (ifelse(ceiling(numb/9)%%9 == 0, 9, ceiling(numb/9)%%9))
-}
-
-mrowki.sudoku.get_rand_raw <- function(numb) {
-  return (ifelse((numb %% 9) == 0, 9, numb%%9))
-}
-
-mrowki.sudoku.get_rand_value <- function(numb) {
-  return (ceiling(numb/81))
-}
-
-## blok funkcji dokonujπcych zmian w iteracji nad macierzπ wspÛ≥czynnikÛw atrakcyjnoúci
-
-mrowki.sudoku.update_attractivity <- function(attr,pn, j_column, i_raw, value) {
-  attr <- mrowki.sudoku.update_attr_for_raw(attr,pn,i_raw,value)
-  attr <- mrowki.sudoku.update_attr_for_column(attr,pn,j_column,value)
-  attr <- mrowki.sudoku.update_attr_for_square(attr,pn,j_column,i_raw,value)
-  return(attr)
-}
-
-mrowki.sudoku.update_attr_for_raw <- function(attr,pn,raw,value,WA=4) {
-  for(i in 1:9) {
-    if(length(pn[raw,i][[1]] != 0))
-      attr[raw,i] = 1/(length(pn[raw,i][[1]]))^WA
-    if(length(pn[raw,i][[1]]) == 0)
-      attr[raw,i] = 0
-  }
-  return(attr)
-}
-
-mrowki.sudoku.update_attr_for_column <- function(attr,pn,column,value,WA=4) {
-  for(j in 1:9) {
-    if(length(pn[j,column][[1]] != 0)) {
-      attr[j,column] = 1/(length(pn[j,column][[1]]))^WA
-      if(length(pn[j,column][[1]]) == 0)
-        attr[j,column] = 0
-    }
-  }
-  return(attr)
-}
-
-mrowki.sudoku.update_attr_for_square <- function(attr,pn,j,i,value,WA=4) {
-  m_seq <- mrowki.sudoku.set_square_iter_seq(i)
-  n_seq <- mrowki.sudoku.set_square_iter_seq(j)
-  for(m in m_seq) {
-    for(n in n_seq) {
-      if(length(pn[n,m][[1]] != 0))
-        attr[n,m] = 1/(length(pn[n,m][[1]]))^WA
-      if(length(pn[n,m][[1]]) == 0)
-        attr[n,m] = 0
-    }
-  }
-  return(attr)
-}
-
-## blok funkcji dokonujπcych zmian nad macierzπ moøliwych ruchÛw dla pÛl
-
-mrowki.sudoku.update_prop_number <- function(pn,j_column,i_raw,value) {
-  pn <- mrowki.sudoku.delete_value_from_raw(pn,i_raw,value)
-  pn <- mrowki.sudoku.delete_value_from_column(pn,j_column,value)
-  pn <- mrowki.sudoku.delete_value_from_square(pn,j_column,i_raw,value)
-  return(pn)
-}
-
-mrowki.sudoku.delete_value_from_raw <- function(pn,raw,value) {
-  for(i in 1:9) {
-    if(length(pn[raw,i][[1]] != 0))
-      pn[raw,i][[1]] <- pn[raw,i][[1]][pn[raw,i][[1]]!=value]
-    if(length(pn[raw,i][[1]]) == 0)
-      pn[raw,i][1] <- list(NULL)
-  }
-  return(pn)
-}
-
-mrowki.sudoku.delete_value_from_column <- function(pn,column,value) {
-  for(j in 1:9) {
-    if(length(pn[j,column][[1]] != 0)) {
-      pn[j,column][[1]] <- pn[j,column][[1]][pn[j,column][[1]]!=value]
-      if(length(pn[j,column][[1]]) == 0)
-        pn[j,column][1] <- list(NULL)
-    }
-  }
-  return(pn)
-}
-
-mrowki.sudoku.delete_value_from_square <- function(pn,j,i,value) {
-  m_seq <- mrowki.sudoku.set_square_iter_seq(i)
-  n_seq <- mrowki.sudoku.set_square_iter_seq(j)
-  for(m in m_seq) {
-    for(n in n_seq) {
-      if(length(pn[n,m][[1]] != 0))
-        pn[n,m][[1]] <- pn[n,m][[1]][pn[n,m][[1]]!=value]
-      if(length(pn[n,m][[1]]) == 0)
-        pn[n,m][1] <- list(NULL)
-    }
-  }
-  return(pn)
+  return (sample(x=Elements,size=1,replace=FALSE,prob=P)[[1]])
 }
 
 mrowki.sudoku.update_state <- function(YS,n_column,n_raw,n_value) {
@@ -211,55 +48,72 @@ mrowki.sudoku.update_state <- function(YS,n_column,n_raw,n_value) {
   return(YS)
 }
 
-mrowki.sudoku.get_sons <- function(ID,M) {
+mrowki.sudoku.get_sons <- function(ID) {
   if(M$vertices[[ID]] != NULL) {
-      return(M$vertices[[ID]]$sons)
+      return(mrowki.vertices[[ID]]$sons)
     
   # if M$vertices[[ID]] == NULL
-  return(mrowki.sudoku.build_sons(ID,M))
+  return(mrowki.sudoku.build_sons(ID))
     
 }
 
-mrowki.sudoku.build_sons <- function(ID,M) {
-  board = M$vertices[[ID]]$board
-  vertices = M$vertices
+mrowki.sudoku.add_son <- function(board) {
+  mrowki.vertices[[length(mrowki.vertices)+1]]$board <<- board
+  mrowki.vertices[[length(mrowki.vertices)+1]]$sons <<- NULL
+}
+
+mrowki.sudoku.build_sons <- function(ID) {
+  board = mrowki.vertices[[ID]]$board
   for(i in 1:9)
     for(j in 1:9)
       if(board[i,j] == 0) {
-        seq = mrowki.sudoku.set_numb_seq(i,j,board)
+        seq = sudoku.set_numb_seq(i,j,board)
         for(k in seq) {
-          newboard = mrowki.sudoku.new_board(i,j,k)
-          for(m in 1:length(vertices)) {
-            mrowki.sudoku.checkBoards(newboard,vertices[[m]]$board)
-            
+          newboard = sudoku.new_board(i,j,k)
+          newID = -1
+          for(m in 1:length(vertices))
+            if(sudoku.check_boards(newboard,mrowki.vertices[[m]]$board)) {
+              newID = m
+              toCreate = false
+              break
+            }
+          
+          if(newID == -1) {
+            mrowki.sudoku.add_son(newboard)
           }
+          
         }
       }
+  
+  return(mrowki.vertices[[ID]]$sons)
       
     
 }
 
-# M - model zawierajacy feromony
-# XS - wybrany ostatni stan
-# PN - ciagi mozliwych liczb do wstawienia w pola
-# AS - stopien atrakcyjnosci
-# WA - wskaznik atrakcyjnosci
+mrowki.sudoku.get_pheromons <- function(ID, sons, pheromons) {
+  prop = list()
+  for(i in 1:length(sons)) {
+    prop[i] = pheromons[[paste(ID,"->",sons[[i]])]]
+  }
+  return(prob)
+}
+
+
 mrowki.sudoku.op_generate <- function(XS,M,UG, WA=4) {
   YS <- mrowki.sudoku.init_state()
   
   ID <- YS[[1]]
   
-  sons <- mrowki.sudoku.get_sons(ID,M)
+  X <- mrowki.sudoku.get_sons(ID)
     
-  while(mrowki.sudoku.there_is_move(M$vertices[[ID]]$sons)) {
-    # zakladam, ze M to macierz 9x9x9 
-    
-    X <- mrowki.sudoku.get_sons()
+  while(mrowki.sudoku.there_is_move(X)) {
+    P <- mrowki.sudoku.get_pheromons(ID,X,M$pheromons)
     
     ID <- mrowki.sudoku.rand_move(X,P)
     
-    
     YS <- mrowki.sudoku.update_state(YS,ID)
+    
+    X <- mrowki.sudoku.get_sons(ID)
   }
   
   return(YS)
@@ -286,11 +140,3 @@ mrowki.sudoku <-function(task){
   
   return (mrowki.search())
 }
-
-print('Rozwiazujemy sudoku')
-mrowki.sudoku.init_state()
-print('Rozwiazane sudoku')
-mrowki.sudoku(mrowki.sudoku.init_state())
-
-mrowki.sudoku.op_generate(s,c(),c())
-mrowki.sudoku.extend_state(mrowki.sudoku.init_state())
