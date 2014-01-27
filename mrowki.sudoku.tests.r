6df6c6752cd8b829b898d859d5c2d3b7c361b3ed
Wez Sie Tato <le0n.uo@gmail.com>
2014-01-27 17:35:25 +0100

    Wywalam niepotrzebne funkcje

--- mrowki.sudoku.tests.r
+++ mrowki.sudoku.tests.r
@@ -11,8 +11,36 @@ mrowki.sudoku.tests.current_fill_max <- -1
 
 mrowki.sudoku.tests.origin_stop_criterion <<- mrowki.stop_criterion
 
-mrowki.sudoku.tests <-function(s,ants = 1,degrad=0.81,start=0.5){
 
+mrowki.sudoku.tests.search<-function(mrowki.sudoku.tests.stop_criterion){
+  search(mrowki.model_init, mrowki.model_update, mrowki.op_init, mrowki.op_select, mrowki.op_generate, mrowki.sudoku.tests.stop_criterion, UG )
+  
+  return(mrowki.solution)
+}
+
+mrowki.sudoku.tests.stop_criterion <- function(XS,M) {
+  if(length(XS) != 1) {
+    mrowki.sudoku.tests.current_fill_max <<- max(mrowki.sudoku.tests.current_fill_max,length(XS[[length(XS)]]) / mrowki.sudoku.tests.max_fill)
+    mrowki.sudoku.tests.fill_levels[length(mrowki.sudoku.tests.fill_levels) + 1] <<- mrowki.sudoku.tests.current_fill_max
+  }
+  
+   return(mrowki.sudoku.tests.origin_stop_criterion(XS,M))
+}
+  
+mrowki.sudoku.tests.set_max_fill <- function(s) {
+  fill <- 1
+  for(i in 1:9) {
+    for(j in 1:9) {
+      if(s[i,j] == 0){
+        fill <- fill + 1
+      }
+    }
+  }
+  return(fill)
+}
+
+mrowki.sudoku.tests <-function(s,ants = 1,degrad=0.81,start=0.5){
+  
   
   mrowki.sudoku.tests.max_fill <<- mrowki.sudoku.tests.set_max_fill(s)
   mrowki.sudoku.tests.fill_levels <<- c()
@@ -46,104 +74,6 @@ mrowki.sudoku.tests <-function(s,ants = 1,degrad=0.81,start=0.5){
   return (mrowki.sudoku.tests.search(mrowki.sudoku.tests.stop_criterion))
 }
 
-mrowki.sudoku.tests.search<-function(mrowki.sudoku.tests.stop_criterion){
-  search(mrowki.model_init, mrowki.model_update, mrowki.op_init, mrowki.op_select, mrowki.op_generate, mrowki.sudoku.tests.stop_criterion, UG )
-  
-  return(mrowki.solution)
-}
-
-mrowki.sudoku.tests.stop_criterion <- function(XS,M) {
-  if(length(XS) != 1) {
-    mrowki.sudoku.tests.current_fill_max <<- max(mrowki.sudoku.tests.current_fill_max,length(XS[[length(XS)]]) / mrowki.sudoku.tests.max_fill)
-    mrowki.sudoku.tests.fill_levels[length(mrowki.sudoku.tests.fill_levels) + 1] <<- mrowki.sudoku.tests.current_fill_max
-  }
-  
-   return(mrowki.sudoku.tests.origin_stop_criterion(XS,M))
-}
-  
-mrowki.sudoku.tests.set_max_fill <- function(s) {
-  fill <- 1
-  for(i in 1:9) {
-    for(j in 1:9) {
-      if(s[i,j] == 0){
-        fill <- fill + 1
-      }
-    }
-  }
-  return(fill)
-}
-
-mrowki.sudoku.tests.test_1 <- function(option=4) {
-  mrowki.sudoku.tests(option)
-  
-  plot(1:length(mrowki.sudoku.tests.fill_levels),mrowki.sudoku.tests.fill_levels,type="l",col="blue",xlab="Iteracja",ylab="Wypelnienie",main="Wykres wypelnienia sudoku w zaleznosci od iteracji")
-  
-}
-
-mrowki.sudoku.tests.test_2 <- function(option=4) {
-  
-  results <- c()
-  
-  for(i in 1:15) {
-    
-    mrowki.sudoku.tests(option)
-    results[length(results)+1] = length(mrowki.sudoku.tests.fill_levels)
-  }
-  
-  plot(1:length(results),results,type="l",col="blue",xlab="Kolejne uruchomienia",ylab="Ilosc krokow",main="Wykres wykonanych ilosci krokow algorytmu w kolejnych uruchomieniach programu")
-  
-}
-
-mrowki.sudoku.tests.test_3 <- function(option=4,antnumber=2) {
-  if(antnumber > 4) {
-    stop("Testy nie przewiduja przypadku wiekszego niz 4 mrowki")
-  }
-
-   
-  resultslist <- list()
-  
-  for(i in 1:antnumber) {
-    resultslist[[i]] <- list()
-    mrowki.antNumber <<- i
-    mrowki.sudoku.tests(option)
-    
-    resultslist[[i]] <- mrowki.sudoku.tests.fill_levels
-  }
-  
-  maxLength <- -1
-  maxIndex <- -1
-  for(i in 1:length(resultslist)) {
-    if(length(resultslist[[i]]) > maxLength) {
-      maxLength <- length(resultslist[[i]])
-      maxIndex <- i
-    }
-  }
-  
-  plot(1:length(resultslist[[maxIndex]]),resultslist[[maxIndex]],type="l",xlab="Iteracja",ylab="Wypelnienie",main="Wykres wypelnienia sudoku w zaleznosci od iteracji")
-  
-  
-  
-  for(i in 1:antnumber) {
-    if(i == 1 && i != maxIndex) {
-      lines(1:length(resultslist[[i]]),resultslist[[i]],col="red")
-    }
-    if(i == 2 && i != maxIndex) {
-      lines(1:length(resultslist[[i]]),resultslist[[i]],col="green")
-    }
-    if(i == 3 && i != maxIndex) {
-      lines(1:length(resultslist[[i]]),resultslist[[i]],col="blue")
-    }
-    if(i == 4 && i != maxIndex) {
-      lines(1:length(resultslist[[i]]),resultslist[[i]],col="yellow")
-    }
-    
-  }
-  
-  
-  #
-  mrowki.antNumber <<- 1
-  #
-}
 
 mrowki.sudoku.tests.test_main <- function(n=20,antnumber=2,degrad=0.81,start=0.5) {
   results <- list()
@@ -151,9 +81,14 @@ mrowki.sudoku.tests.test_main <- function(n=20,antnumber=2,degrad=0.81,start=0.5
   
   
   for(i in 1:15) {
-    
+    print('Sudoku do rozwiazania:')
     mrowki.sudoku.tests(mrowki.sudoku.tests.generate_option(n),antnumber,degrad,start)
+    print("Rozwiazanie:")
+    print(mrowki.solution)
     results[[i]] = mrowki.sudoku.tests.fill_levels
+    print('=========================')
+    
+   
   }
   
   maxLength <- -1
@@ -165,7 +100,7 @@ mrowki.sudoku.tests.test_main <- function(n=20,antnumber=2,degrad=0.81,start=0.5
     }
   }
   
-  plot(1:length(results[[maxIndex]]),results[[maxIndex]],type="l",col="blue",xlab="Kolejne uruchomienia",ylab="Ilosc krokow",ylim=c(0.5,1), xlim=c(1,length(results[[maxIndex]])),main="Wykres wykonanych ilosci krokow algorytmu w kolejnych uruchomieniach programu")
+  plot(1:length(results[[maxIndex]]),results[[maxIndex]],type="l",col="blue",xlab="Numer uruchomienia",ylab="Funkcja celu (wypełnienie sudoku)",ylim=c(0.5,1), xlim=c(1,length(results[[maxIndex]])),main="Wykres wykonanych ilosci krokow algorytmu w kolejnych uruchomieniach programu")
   
   for(i in 1:15) {
     if(i != maxIndex) {
@@ -174,100 +109,6 @@ mrowki.sudoku.tests.test_main <- function(n=20,antnumber=2,degrad=0.81,start=0.5
   }
 }
 
-
-mrowki.sudoku.tests.get_option <- function(option) {
-  s = c()
-  
-  if(option == 1) {
-    # Z jednym  
-    s[1:9] =   c(1,2,3,4,5,6,7,8,9)
-    s[10:18] = c(4,5,6,7,8,9,1,2,3)
-    s[19:27] = c(7,8,9,1,2,3,4,5,6)
-    s[28:36] = c(2,1,4,3,6,5,8,9,7)
-    s[37:45] = c(3,6,5,8,9,7,2,1,4)
-    s[46:54] = c(8,9,7,2,1,4,3,6,5)
-    s[55:63] = c(5,3,1,6,4,2,9,7,8)
-    s[64:72] = c(6,4,2,9,7,8,5,3,1)
-    s[73:81] = c(9,7,8,5,3,1,6,4,0)
-  }
-  if(option == 2) {
-    # z dwoma
-    s[1:9] =   c(1,2,3,4,5,6,7,8,9)
-    s[10:18] = c(4,5,6,7,8,9,1,2,3)
-    s[19:27] = c(7,8,9,1,2,3,4,5,6)
-    s[28:36] = c(2,1,4,3,6,5,8,9,7)
-    s[37:45] = c(3,6,5,8,9,7,2,1,4)
-    s[46:54] = c(8,9,7,2,1,4,3,6,5)
-    s[55:63] = c(5,3,1,6,4,2,9,7,8)
-    s[64:72] = c(6,4,2,9,7,8,5,3,1)
-    s[73:81] = c(9,7,8,5,3,1,0,0,0)
-  }
-  if(option == 3) {
-    # bez kolumny
-    s[1:9] =   c(1,2,3,4,5,6,7,8,9)
-    s[10:18] = c(4,5,6,7,8,9,1,2,3)
-    s[19:27] = c(7,8,9,1,2,3,4,5,6)
-    s[28:36] = c(2,1,4,3,6,5,8,9,7)
-    s[37:45] = c(3,6,5,8,9,7,2,1,4)
-    s[46:54] = c(8,9,7,2,1,4,3,6,5)
-    s[55:63] = c(5,3,1,6,4,2,9,7,8)
-    s[64:72] = c(6,4,2,9,7,8,5,3,1)
-    s[73:81] = c(0,0,0,0,0,0,0,0,0)
-  }
-  if(option == 4) {
-    # dwie puste kolumny
-    s[1:9] =   c(1,2,3,4,5,6,7,8,9)
-    s[10:18] = c(4,5,6,7,8,9,1,2,3)
-    s[19:27] = c(7,8,9,1,2,3,4,5,6)
-    s[28:36] = c(2,1,4,3,6,5,8,9,7)
-    s[37:45] = c(3,6,5,8,9,7,2,1,4)
-    s[46:54] = c(8,9,7,2,1,4,3,6,5)
-    s[55:63] = c(5,3,1,6,4,2,9,7,8)
-    s[64:72] = c(0,0,0,0,0,0,0,0,0)
-    s[73:81] = c(0,0,0,0,0,0,0,0,0)
-  }
-  if(option == 5) {
-    # prawie wype?niona
-    s[1:9] =   c(1,2,3,4,5,6,7,8,9)
-    s[10:18] = c(4,5,0,7,8,0,1,2,3)
-    s[19:27] = c(7,8,0,1,2,3,4,5,6)
-    s[28:36] = c(2,1,4,3,6,0,8,9,7)
-    s[37:45] = c(0,6,5,0,9,7,2,1,4)
-    s[46:54] = c(8,9,0,2,1,4,3,6,5)
-    s[55:63] = c(5,3,1,6,4,2,9,7,8)
-    s[64:72] = c(0,0,0,0,0,0,0,0,0)
-    s[73:81] = c(0,0,8,5,3,1,0,0,0)
-  }
-  if(option == 6) {
-    #prawie niewype?niona
-    s[1:9] =   c(0,5,0,0,0,6,8,0,0)
-    s[10:18] = c(0,0,3,0,8,1,7,0,0)
-    s[19:27] = c(0,0,7,4,3,0,0,1,0)
-    s[28:36] = c(7,0,0,0,0,4,1,5,8)
-    s[37:45] = c(3,0,0,0,7,0,0,0,2)
-    s[46:54] = c(2,1,5,9,0,0,0,0,7)
-    s[55:63] = c(0,7,0,0,4,9,3,0,0)
-    s[64:72] = c(0,0,9,8,5,0,2,0,0)
-    s[73:81] = c(0,0,8,2,0,0,0,9,0)
-  }
-  if(option == 7) {
-    #pusta
-    s[1:9] =   c(0,0,0,0,0,0,0,0,0)
-    s[10:18] = c(0,0,0,0,0,0,0,0,0)
-    s[19:27] = c(0,0,0,0,0,0,0,0,0)
-    s[28:36] = c(0,0,0,0,0,0,0,0,0)
-    s[37:45] = c(0,0,0,0,0,0,0,0,0)
-    s[46:54] = c(0,0,0,0,0,0,0,0,0)
-    s[55:63] = c(0,0,0,0,0,0,0,0,0)
-    s[64:72] = c(0,0,0,0,0,0,0,0,0)
-    s[73:81] = c(0,0,0,0,0,0,0,0,0)
-  }
-  if(is.null(s)) {
-    stop("Podano bledny parametr option")
-  }
-  dim(s) = c(9,9)
-  return (s)
-}
 
 mrowki.sudoku.tests.generate_option <- function(n) {
   if(n<1) {
